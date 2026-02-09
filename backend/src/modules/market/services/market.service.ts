447e42d653302a6320fb49675bcb983ce5df4982
Pawan Talekar <pawantalekar2@gmail.com>
2026-02-09 11:14:10 +0530

    Briefly-1009-adding headers with the api request

--- backend/src/modules/market/services/market.service.ts
+++ backend/src/modules/market/services/market.service.ts
@@ -21,6 +21,10 @@ export class MarketService {
                     per_page: 10,
                     page: 1,
                     sparkline: false
+                },
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Accept': 'application/json'
                 }
             });
 
